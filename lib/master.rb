b70e6a70659f3e04e853766b3fd4f911bd81a75c
Korotaev Danil <greyd@mail333.com>
2013-08-21 03:49:27 +0600

    изменена логика - один тип воркера на все запросы, добавлены спеки

--- lib/master.rb
+++ lib/master.rb
@@ -1,25 +1,21 @@
 # encoding : utf-8
 require 'socket'
 
-class Master < Process
+class Master
   PORT = 80
   HOST = 'localhost'
-  RECEIVERS_COUNT = 2
-  SENDERS_COUNT = 2
+  WORKERS_COUNT = 5
 
-  def initialize
-    @manager = WorkerManager.new :file_receive => RECEIVERS_COUNT,
-                                 :file_send    => SENDERS_COUNT
+  def initialize(workers_count = WORKERS_COUNT)
+    @manager = WorkerManager.new(workers_count)
     @listener = TCPServer.new(HOST, PORT)
-    listen
+    @parser = Parser.new
   end
-
-  private
 
   def listen
     loop do
-      request = Request.new(@listener.accept)
-      1 until @manager.work request
+      request = @parser.parse(@listener.accept)
+      @manager.work request
     end
   end
 end
--- lib/worker.rb
+++ lib/worker.rb
@@ -1,5 +1,21 @@
 # encoding : utf-8
 
-class Worker < Process
+class Worker
+  attr_reader :pid
 
+  # Public: Отправить запрос воркеру
+  #
+  # request - Request
+  #
+  # Returns nothing
+  def send!(request)
+
+  end
+
+  # Public: Принять запрос от мастера
+  #
+  # Returns Request
+  def receive!
+
+  end
 end
