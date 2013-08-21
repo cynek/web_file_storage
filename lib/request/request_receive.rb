28bd745cbf68324b2642e51fe8616b323d39dbbc
SemenMolokanov <molokanovcv@yandex.com>
2013-08-21 16:32:20 +0600

    переделали request в task

--- lib/request/request_receive.rb
+++
@@ -1,8 +0,0 @@
-# encoding : utf-8
-require './request'
-
-class RequestReceive < Request
-  # класс запроса на закачку файлов на сервер
-  attr_accessor :content_length, # объем данных
-                :boundary        # разделитель файлов
-end # class RequestReceive
