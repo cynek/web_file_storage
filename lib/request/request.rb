c6ca8c10ec593d908b196b38f5f211c188ff1551
SemenMolokanov <molokanovcv@yandex.com>
2013-08-21 12:59:56 +0600

    accept range, refactor requests

--- lib/request/request.rb
+++ lib/request/request.rb
@@ -1,3 +1,5 @@
+# encoding : utf-8
+
 class Request
   # базовый класс для RequestSend и RequestReceive
   attr_accessor :socket
@@ -6,15 +8,3 @@ class Request
     yield(self)
   end
 end # class Request
-
-class RequestSend < Request
-  # класс запроса на скачивание файла с сервера
-  attr_accessor :file_path,     # путь к файлу  
-                :seek_position  # смещение от начала файла для докачки
-end # class RequestSend
-
-class RequestReceive < Request
-  # класс запроса на закачку файлов на сервер
-  attr_accessor :content_length, # объем данных
-                :boundary        # разделитель файлов
-end # class RequestReceive
