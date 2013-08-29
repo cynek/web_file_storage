ff00c2bcd4fc629722a222dc1e190808358879af
SemenMolokanov <molokanovcv@yandex.com>
2013-08-29 16:19:25 +0600

    task builder

--- lib/request/parser.rb
+++ lib/request/parser.rb
@@ -1,65 +1,63 @@
 # encoding : utf-8
 
 module Parser
-  class Parser    
-    # Parser возвращает request_method, uri, header полученные из io
-    #
-    # Example:
-    #
-    #  # socket: IO object
-    #  # request_method: String
-    #  # request_method: String
-    #  # header: Hash
-    #  request_method, uri, header = Parser::Parser.parse(socket)
-    
-    def self.parse(socket)
-      # парсим данные, полученные в socket
-      request_method, uri = parse_request_line(socket)
-      headers = parse_headers(socket)
-      return request_method, uri, headers
+# Parser.parse возвращает request_method, uri, header полученные из io
+#
+# Example:
+#
+#  # socket: IO object
+#  # request_method: String
+#  # request_method: String
+#  # header: Hash
+#  request_method, uri, header = Parser.parse(socket)
+
+  def self.parse(socket)
+    # парсим данные, полученные в socket
+    request_method, uri = parse_request_line(socket)
+    headers = parse_headers(socket)
+    return request_method, uri, headers
+  end
+
+  private
+
+  class << self
+    def parse_request_line(socket)
+      # определить метод и URI
+      request_line = socket.gets.chomp
+
+      regex = %r[\A(?<request_method>\S+)\s+    # GET, POST, etc
+                 (?<uri>\/\S+)\s+\S+]x          # URI & tail
+
+      match_data = regex.match request_line
+      if match_data
+        request_method = match_data[:request_method]
+        uri = match_data[:uri]
+      end
+
+      [request_method, uri]
     end
 
-    private
+    def parse_headers(socket)
+      # парсим заголовки
+      header = Hash.new { |h, k| h[k] = [] }
 
-    class << self
-      def parse_request_line(socket)
-        # определить метод и URI
-        request_line = socket.gets.chomp
+      empty_line = /\A\r?\n\z/
+      header_regex = %r[^(?<header_name>[A-Za-z0-9!\#$%&'*+\-.^_`|~]+): # header name
+                        \s*(?<header_value>.*?)\s*\z]x                  # header value
 
-        regex = %r[\A(?<request_method>\S+)\s+    # GET, POST, etc
-                   (?<uri>\/\S+)\s+\S+]x          # URI & tail
+      loop do
+        line = socket.gets
+        break if empty_line.match(line) || line.nil?
 
-        match_data = regex.match request_line 
+        match_data = header_regex.match line
         if match_data
-          request_method = match_data[:request_method]
-          uri = match_data[:uri]
+          name = match_data[:header_name].chomp
+          value = match_data[:header_value].chomp
+          header[name] << value
         end
-
-        [request_method, uri]
       end
 
-      def parse_headers(socket)
-        # парсим заголовки
-        header = Hash.new { |h, k| h[k] = [] }
-        
-        empty_line = /\A\r?\n\z/
-        header_regex = %r[^(?<header_name>[A-Za-z0-9!\#$%&'*+\-.^_`|~]+): # header name
-                          \s*(?<header_value>.*?)\s*\z]x                  # header value
-        
-        loop do
-          line = socket.gets
-          break if empty_line.match(line) || line.nil?
-
-          match_data = header_regex.match line 
-          if match_data
-            name = match_data[:header_name].chomp
-            value = match_data[:header_value].chomp            
-            header[name] << value
-          end                 
-        end
-
-        header
-      end
+      header
     end
-  end # class Parser
+  end
 end # module Parser
