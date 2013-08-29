777d1e1114b8b2fe5498a80af340027953f14c84
SemenMolokanov <molokanovcv@yandex.com>
2013-08-29 16:19:25 +0600

    task builder

--- bin/start.rb
+++ bin/start.rb
@@ -1,18 +1,17 @@
 # encoding : utf-8
 
-Dir[File.join("../#{File.dirname(__FILE__)}", 'lib/request', '*.rb')].each {|file| require file }
-
+Dir[File.join("../#{File.dirname(__FILE__)}", 'lib/request', '*.rb')].each { |file| require file }
 
 puts 'Check parser:'
 puts
 File.open('../examples/http_post.txt', 'r') do |file|
-  puts Parser::Parser.parse(file)
+  puts Parser.parse(file)
 end
 
 puts
 
 File.open('../examples/http_get.txt', 'r') do |file|
-  puts Parser::Parser.parse(file)
+  puts Parser.parse(file)
 end
 
 puts
