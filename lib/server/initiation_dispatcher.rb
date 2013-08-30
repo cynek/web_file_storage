3a604e60c489e27dfd22af093cca1bff4a6d843a
Korotaev Danil <greyd@mail333.com>
2013-08-31 00:20:14 +0600

    Добавлена поддержка нескольких обработчиков на одном соединении

--- lib/server/initiation_dispatcher.rb
+++ lib/server/initiation_dispatcher.rb
@@ -1,5 +1,6 @@
 # encoding : utf-8
 require 'singleton'
+require 'set'
 
 module Server
   # Регистриует и уведомляет хэндлеры о событиях
@@ -7,27 +8,55 @@ module Server
     include Singleton
 
     def initialize
-      @handlers = {}
+      # обработчики событий для каждого сокета
+      @socket_handlers = Hash.new do |events, socket|
+        events[socket] = Hash.new {|handlers, event| handlers[event] = Set.new }
+      end
+
       @epoll = SP::Epoll.new
     end
 
-    def register_handler(event_handler, event_type)
+    def register_handler(event_handler, event_types)
       socket = event_handler.get_handle
+      flags = event_types.inject(0) {|flags, event| flags | event }
 
-      @handlers[socket] = event_handler
-      @epoll.add(socket, event_type)
+      if @socket_handlers.include?(socket)
+        @epoll.mod(socket, @epoll.events_for(socket) | flags)
+      else
+        @epoll.add(socket, flags)
+      end
+
+      event_types.each do |event_type|
+        @socket_handlers[socket][event_type] << event_handler
+      end
     end
 
-    def remove_handler(event_handler)
-      socket = event_handler.get_handle
+    def remove_handler(event_handler, event_type)
+      # TODO: сделать возможность передавать несколько событий
 
-      @handlers.delete socket
-      @epoll.del(socket)
+      socket = event_handler.get_handle
+      @socket_handlers[socket][event_type].delete event_handler
+
+      # если на сокете не осталось других обработчиков этого типа
+      if @socket_handlers[socket][event_type].empty?
+        @socket_handlers[socket].delete(event_type)
+
+        # отключаем наблюдение за сокетом, если обрабочиков на нем больше нет
+        if @socket_handlers[socket].empty?
+          @socket_handlers.delete socket
+          @epoll.del(socket)
+        # иначе отключаем наблюдение только за событием на этом сокете
+        else
+          @epoll.mod(socket, @epoll.events_for(socket) & ~event_type)
+        end
+      end
     end
 
     def handle_events(timeout = 0)
       @epoll.wait do |event, socket|
-        @handlers[socket].handle_event event
+        @socket_handlers[socket][event].each do |handler|
+          handler.handle_event event
+        end
       end
     end
   end
--- lib/server/read_handler.rb
+++ lib/server/read_handler.rb
@@ -4,7 +4,7 @@ module Server
   class ReadHandler < EventHandler
     def initialize(connection_socket)
       @connection = connection_socket
-      InitiationDispatcher.instance.register_handler(self, SP::Epoll::IN | SP::Epoll::ET)
+      InitiationDispatcher.instance.register_handler(self, [SP::Epoll::IN, SP::Epoll::ET, SP::Epoll::OUT])
     end
 
     def get_handle
@@ -14,9 +14,17 @@ module Server
     def handle_event(event)
       case event
         when SP::Epoll::IN
-          puts @connection.recv(255)
-          InitiationDispatcher.instance.remove_handler self
-          @connection.close
+          received_str = (@connection.recv(255)).strip
+          if received_str == 'END'
+            InitiationDispatcher.instance.remove_handler self, SP::Epoll::IN
+            InitiationDispatcher.instance.remove_handler self, SP::Epoll::ET
+            @connection.close
+          else
+            puts "RECEIVED #{received_str} SIZE #{received_str.size}"
+          end
+        when SP::Epoll::OUT
+        # TODO: разобраться почему блокируется ввод при этом событии
+          @connection.puts "RECEIVED BY #{Process.pid}"
       end
     end
   end
