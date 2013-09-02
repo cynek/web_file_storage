6ca791240020df2224ae15999088b8cee916098b
SemenMolokanov <molokanovcv@yandex.com>
2013-09-02 12:46:52 +0600

    rack & server

--- bin/start.rb
+++ bin/start.rb
@@ -7,13 +7,13 @@ require 'rubygems'
 require 'rack'
 
 class FileApp
+
   def call(env)
     puts "process #{Process.pid} \nenv: #{env}"
-
     request = Parser.parse(env[:socket])
     #TODO приложение для отправки файлов в body
 
-    [200, request[:headers], ['get from rack!']]
+    [200, {'Content-Type' => ['text/html; charset=utf-8']}, ['get from rack!']]
   end
 
 end
